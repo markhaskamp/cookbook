7cded6568d317e221957230210ceeabec2d07b6e
Mark Haskamp <mark.haskamp@pinnsg.com>
2016-01-17 15:55:40 -0500

    Readfile

--- go/files/src/slurp/slurp_whole_file_test.go
+++ go/files/src/slurp/slurp_whole_file_test.go
@@ -1,11 +1,13 @@
 package slurp
 
 import "fmt"
+import "strings"
 import "testing"
 
 
 func Test_Readfile_returns_a_uint8_array(t *testing.T) {
 
+  // byte is an alias of uint8
   expected := "[]uint8"
 
   lines,_ := slurp("foo.dat")
@@ -15,3 +17,17 @@ func Test_Readfile_returns_a_uint8_array(t *testing.T) {
     t.Fail()
   }
 }
+
+func Test_Readfile_returns_the_whole_file(t *testing.T) {
+  expected := 9
+
+  fileByteArray,_ := slurp("foo.dat")
+  lines := strings.Split(string(fileByteArray), "\n")
+  actual := len(lines)
+
+  if expected != actual {
+    fmt.Println("expected: ", expected, "--- actual: ", actual)
+    t.Fail()
+  }
+}
+
